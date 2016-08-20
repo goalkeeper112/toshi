20cf6d0b62a723fd119fdec78b9fad8c31bba6f9
Gary Rafferty <gary.rafferty@gmail.com>
2016-08-20 17:36:56 +0100

    Restructure client class

--- lib/toshi.rb
+++ lib/toshi.rb
@@ -2,29 +2,23 @@ require "toshi/version"
 
 require "httparty"
 
+require "toshi/api_resource"
 require "toshi/block"
 require "toshi/request"
 
 module Toshi
   class Client
-    attr_reader :base_url
+    @@base_url = "http://bitcoin.toshi.io/api/v0"
 
-    def initialize
-      @base_url = "http://bitcoin.toshi.io/api/v0"
-    end
+    class << self
+      def get(path)
+        url = url_for(path)
+        Toshi::Request.get(url)
+      end
 
-    def get(path)
-      url = url_for(path)
-      Toshi::Request.get(url)
-    end
-
-    def blocks
-      Toshi::Block.new(self)
-    end
-
-    private
-    def url_for(path)
-      @base_url + path
+      def url_for(path)
+        @@base_url + path
+      end
     end
   end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,20 +1,11 @@
 require 'spec_helper'
 
 describe Toshi::Client do
-  let(:client) { described_class.new }
+  let(:client) { described_class }
   let(:base) { 'http://bitcoin.toshi.io/api/v0' }
 
-  it 'sets base url on instantiation' do
-    expect(client.base_url).to eq(base)
-  end
-
-  it 'delegates GET requests to Request class' do
+  it 'delegates get requests to Request class' do
     expect(Toshi::Request).to receive(:get).with(base+'/path')
     client.get('/path')
-  end
-
-  it 'delegates blocks to Block class' do
-    expect(Toshi::Block).to receive(:new).with(client)
-    client.blocks
   end
 end

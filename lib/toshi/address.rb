fa319e95e7e1f35a0104d732fe4f4e04f00b9ec5
Luis Fernando <lfbu.112@gmail.com>
2016-09-27 22:19:09 -0400

    Update address.rb

--- lib/toshi/address.rb
+++ lib/toshi/address.rb
@@ -7,7 +7,8 @@ module Toshi
       end
       
       def unspent(identifier)
-        response = client.get '/addresses/'+identifier.to_s+'/unspent_outputs'
+        puts identifier.to_s
+        response = client.get '/addresses/#{identifier.to_s}/unspent_outputs/'
         create_many(response)
       end
     end
